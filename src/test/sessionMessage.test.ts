87ea82018154b5e2c8f6d04a59bccd4026245984
Seulgi Kim <seulgi.kim@kodebox.io>
2019-01-28 19:32:45 +0900

    Remove SecretDenied and NonceDenied

--- src/test/sessionMessage.test.ts
+++ src/test/sessionMessage.test.ts
@@ -37,35 +37,6 @@ describe("Check Session Message RLP encoding", () => {
         ]);
     }, 10000);
 
-    test("NonceDenied RLP encoding test", () => {
-        const body = new sessionMessage.NonceDenied("connection denied");
-        const msg = new sessionMessage.SessionMessage(0, new U256(6), body);
-        expect([...msg.rlpBytes()]).toEqual([
-            213,
-            128,
-            6,
-            8,
-            145,
-            99,
-            111,
-            110,
-            110,
-            101,
-            99,
-            116,
-            105,
-            111,
-            110,
-            32,
-            100,
-            101,
-            110,
-            105,
-            101,
-            100
-        ]);
-    }, 10000);
-
     test("SecretRequest RLP encoding test", () => {
         const body = new sessionMessage.SecretRequest(
             new H512(
